3da8cb7910e0285c6b15b56e2845f9e539c8860f
darkostojan0v <95142705+darkostojan0v@users.noreply.github.com>
2022-06-19 21:56:40 +0200

    Homework01

---
+++ SEDC.PizzaApp_Homework1/SEDC.PizzaApp/Controllers/OrderController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SEDC.PizzaApp.Models;
+
+namespace SEDC.PizzaApp.Controllers
+{
+    public class OrderController : Controller
+    {
+
+        [Route("ListOrders")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new EmptyResult();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public IActionResult JsonData()
+        {
+            Pizzerias pizzeria = new Pizzerias()
+            {
+                Id = 1,
+                Name = "Jakomo"
+            };
+
+            return new JsonResult(pizzeria);
+        }
+
+        public IActionResult Redirect()
+        {
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ SEDC.PizzaApp_Homework1/SEDC.PizzaApp/Models/Pizzerias.cs
@@ -0,0 +1,8 @@
+﻿namespace SEDC.PizzaApp.Models
+{
+    public class Pizzerias
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
